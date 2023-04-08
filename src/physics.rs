aeff1b9abbf38f2d8d38abc9cb9fac4e8ba09833
Benny Nazimov <benick12@gmail.com>
2023-04-08 12:32:09 +0300

    Remove physics debug plugin

--- src/physics.rs
+++ src/physics.rs
@@ -38,7 +38,7 @@ pub struct PhysicsHooksResource(Box<dyn PhysicsHooks + Send + Sync>);
 impl Plugin for PhysicsPlugin {
     fn build(&self, app: &mut App) {
         app.add_system(spawn_obstacles.in_schedule(OnEnter(AppState::InGame)))
-            .add_plugin(RapierDebugRenderPlugin::default())
+            // .add_plugin(RapierDebugRenderPlugin::default())
             .add_plugin(RapierPhysicsPlugin::<CustomCollisionHook>::pixels_per_meter(100.0))
             .add_systems(
                 (emit_platforms, despawn_out_of_screen_platforms)
